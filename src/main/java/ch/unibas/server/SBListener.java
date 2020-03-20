d8eb3d5a8aff1cf4c0dc2752f979b3408407b5a9
Guillaume <guillaume.joyet@unibas.ch>
2020-03-20 18:18:05 +0100

    First drafts of server classes.

---
+++ src/main/java/ch/unibas/server/SBListener.java
@@ -0,0 +1,40 @@
+package ch.unibas.server;
+
+import ch.unibas.userExceptions.NoNameException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Thread waiting for any action from client.
+ */
+public class SBListener implements Runnable {
+    private PrintWriter pw;
+    private BufferedReader br;
+
+    public SBListener(PrintWriter pw, BufferedReader br) {
+        this.pw = pw;
+        this.br = br;
+    }
+
+    public void run() {
+        String name = takeName();
+
+    }
+
+    static String takeName() {
+        String name;
+
+        try {
+            // Sende Befehl für System.out.println("Please enter nickname:") auf terminal von Client;
+            // Suche nach unerlaubten Zeichen im Namen -> throw NoNameException
+        } catch(NoNameException) {
+
+        } catch(IOException) {
+
+        }
+
+        return name;
+    }
+}

ce1e64212dfd5f5adf04c8aca8577fc5122ac855
Manuela Wildi <manuela.wildi@stud.unibas.ch>
2020-03-21 14:53:31 +0100

    updated client: client can choose name

--- src/main/java/skipBo/client/SBClient.java
+++ src/main/java/skipBo/client/SBClient.java
@@ -1,5 +1,7 @@
 package skipBo.client;
 
+import skipBo.userExceptions.NoNameException;
+
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -10,16 +12,17 @@ import java.util.Scanner;
  * Starts SBClientListener, handles Server input
  */
 public class SBClient {
-
     public static void main(String[] args) {
+
         try {
             System.out.println("Connecting to port " + args[0] + "…");
             Socket sock = new Socket(args[1], Integer.parseInt(args[0]));
+            Scanner scanner = new Scanner(System.in);
 
-            setName();
+            setName(scanner);
 
             //Start SBClientListener Thread
-            SBClientListener clientListener = new SBClientListener(sock);
+            SBClientListener clientListener = new SBClientListener(sock, scanner);
             Thread listener = new Thread(clientListener);
             listener.start();
 
@@ -30,12 +33,39 @@ public class SBClient {
         }
     }
 
-    static void setName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your name: ");
-        String name = scanner.nextLine();
+    static void setName(Scanner scanner) {
+        boolean nameIsValid = false;
 
+        while(!nameIsValid) {
+            System.out.println("Please enter your name: ");
+            String name = scanner.nextLine();
+            try {
+                isNameValid(name);
+                nameIsValid = true;
+            } catch (NoNameException e) {
+                System.out.println("Name not valid.");
+            }
+
+        }
 
     }
+
+    /*
+    Name is not allowed to have more than 13 characters. Name cannot contain symbol §. Name cannot be empty.
+     */
+    static void isNameValid(String name) throws NoNameException {
+        if (name.length() > 13) {
+            throw new NoNameException();
+        }
+        if (name.contains("§")) {
+            throw new NoNameException();
+        }
+        if (name.isEmpty()) {
+            throw new NoNameException();
+        }
+
+    }
+
+
 
 }

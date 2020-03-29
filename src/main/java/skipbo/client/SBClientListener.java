560eecc0c7a595c28dcc69ccf47ada1f8b19ef7c
Manuela Wildi <manuela.wildi@stud.unibas.ch>
2020-03-29 19:20:40 +0200

    started implementing /play command in SBClientListener

--- src/main/java/skipbo/client/SBClientListener.java
+++ src/main/java/skipbo/client/SBClientListener.java
@@ -54,7 +54,7 @@ class SBClientListener implements Runnable {
     void printCommandInfo() {
         //List of Commands
         String listOfCommands = "Commands:\n/change name [name]\n/change status [ready|waiting]\n" +
-                "/msg [name] [message]\n/new game\n/quit\n";
+                "/msg [name] [message]\n/new game\n/play [PlaceFrom] [n] [PlaceTo] [n] | not yet implemented\n/quit\n";
         System.out.println(listOfCommands);
     }
 
@@ -62,7 +62,7 @@ class SBClientListener implements Runnable {
      * Let's client set their name
      */
     void setName() {
-        System.out.println("Name can only contain letters or digits (no umlaut) and must have between 3 and 13 characters");
+        System.out.println("Name can only contain letters or digits and must have between 3 and 13 characters");
         System.out.println("Your suggested nickname (according to your system username): " + System.getProperty("user.name"));
         System.out.println("Please enter your name: ");
         String name = scanner.nextLine();
@@ -102,6 +102,9 @@ class SBClientListener implements Runnable {
             case "/new":
                 protocolString = getNewString(input);
                 break;
+            case "/play":
+                protocolString = getPlayString(input);
+                break;
             case "/quit":
                 protocolString = Protocol.LGOUT + "";
                 pw.println(protocolString);
@@ -123,9 +126,7 @@ class SBClientListener implements Runnable {
 
         String[] line = input.split(" ",3);
 
-        if (line.length < 3) {
-            throw new NotACommandException("Please add an argument to your command");
-        }
+        if (line.length < 3) throw new NotACommandException("Please add an argument to your command");
 
         String option = line[1];
         String argument = line[2];
@@ -152,7 +153,9 @@ class SBClientListener implements Runnable {
         if (line.length < 3) {
             throw new NotACommandException("Please add a name and a message to your command");
         }
-        return Protocol.CHATM + "§Private§" +line[1] + "§" + line[2];
+        String name = line[1];
+        String message = line[2];
+        return Protocol.CHATM + "§Private§" + name + "§" + message;
     }
 
     /**
@@ -170,6 +173,31 @@ class SBClientListener implements Runnable {
             return Protocol.NWGME + "§New";
         }
         throw new NotACommandException("Please enter a valid command");
+    }
+
+    String getPlayString(String input) throws NotACommandException {
+        String[] line = input.split(" ", 5);
+        if (line.length < 4) {
+            throw new NotACommandException("Usage: /play [PlaceFrom] [n] [PlaceTo] [n]");
+        }
+        String placeFrom = line[1];
+        String fromNumber; //probably can change to int later
+        String placeTo;
+        String toNumber; //probably can change to int later
+        if (placeFrom.equalsIgnoreCase("StockPile")) {
+            fromNumber = "-1";
+            placeTo = line[2];
+            toNumber = line[3];
+        } else {
+            if (line.length < 5) {
+                throw new NotACommandException("Usage: /play [PlaceFrom] [n] [PlaceTo] [n]");
+            }
+            fromNumber = line[2];
+            placeTo = line[3];
+            toNumber = line[4];
+        }
+        throw new NotACommandException("Command not implemented"); //delete when implemented
+        // TODO build and return correct String
     }
 
     /**

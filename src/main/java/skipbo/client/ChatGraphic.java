0c9abe89e7e5032a83ba0b3115c5c9696aee0913
Janni Batsilas <user.email>
2020-03-29 19:27:49 +0200

    Added Listener to CHatGraphic class

--- src/main/java/skipbo/client/ChatGraphic.java
+++ src/main/java/skipbo/client/ChatGraphic.java
@@ -5,14 +5,29 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
 public class ChatGraphic extends JFrame implements Runnable, ActionListener {
+
+
+
 
     private JPanel contentPane;
     private JButton sendMes;
     private JTextArea inputMes;
-    private JScrollPane scrollPane;
+    private JTextField inputMesPrint;
     private SBClientListener clientListener;
+
+
+    public static void main(String[] args) throws IOException {
+        ChatGraphicObsolete frame = new ChatGraphicObsolete();
+        frame.setVisible(true);
+
+    }
+
+
 
     ChatGraphic(SBClientListener clientListener) {
         setFrame();
@@ -34,7 +49,7 @@ public class ChatGraphic extends JFrame implements Runnable, ActionListener {
 
         setTitle("Skip-Bros CHAT");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 520, 485);
+        setBounds(20, 30, 240, 400);
 
         contentPane = new JPanel();
         contentPane.setBackground(Color.black);
@@ -42,10 +57,7 @@ public class ChatGraphic extends JFrame implements Runnable, ActionListener {
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        scrollPane = new JScrollPane();
-        scrollPane.setBounds(20, 30 ,250, 400 );
-        scrollPane.setVisible(true);
-        contentPane.add(scrollPane);
+
 
         sendMes = new JButton("Send message");
         sendMes.setBounds(290, 405, 200,25);
@@ -58,14 +70,24 @@ public class ChatGraphic extends JFrame implements Runnable, ActionListener {
         inputMes.setColumns(3);
         inputMes.setLineWrap(true);
         contentPane.add(inputMes);
+
+        inputMesPrint = new JTextField();
+        inputMesPrint.setBounds(290, 335,200, 50);
+        inputMesPrint.setText("fefe");
+        inputMesPrint.setFont(new Font("Arial",Font.BOLD, 13));
+        inputMesPrint.setVisible(true);
+        contentPane.add(inputMesPrint);
     }
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         System.out.println("entered actionPerformed method"); //For testing purpose
+        inputMesPrint.setText("Dis is önli för testing pörpöses");
         if (actionEvent.getSource() == sendMes) {
             String input = inputMes.getText();
+            inputMesPrint.setText(input);
             try {
+
                 clientListener.forward(input);
             } catch (IndexOutOfBoundsException | NotACommandException e) {
                 System.out.println("Please enter a valid command");

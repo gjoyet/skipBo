55d16ad154bd3d2837f3584cccc69e51934d5711
Manuela Wildi <manuela.wildi@stud.unibas.ch>
2020-03-31 18:36:10 +0200

    chat messages can now be sent by hitting enter bug fixed: whole message is now sent

--- src/main/java/skipbo/client/ChatGraphic.java
+++ src/main/java/skipbo/client/ChatGraphic.java
@@ -5,8 +5,10 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class ChatGraphic extends JFrame implements ActionListener {
+public class ChatGraphic extends JFrame implements KeyListener { //ActionListener
 
     private SBClientListener clientListener;
     private JPanel contentPane;
@@ -14,7 +16,7 @@ public class ChatGraphic extends JFrame implements ActionListener {
     private JTextArea inputMes;
     private JScrollPane chatScrollPane;
     private JScrollPane inputScrollPane;
-    private JButton sendMes;
+    //private JButton sendMes;
 
     ChatGraphic(SBClientListener clientListener) {
         this.clientListener = clientListener;
@@ -53,6 +55,7 @@ public class ChatGraphic extends JFrame implements ActionListener {
         inputMes.setColumns(3);
         inputMes.setLineWrap(true);
         inputMes.setWrapStyleWord(true);
+        inputMes.addKeyListener(this);
         contentPane.add(inputMes);
 
         inputScrollPane = new JScrollPane(inputMes);
@@ -60,17 +63,18 @@ public class ChatGraphic extends JFrame implements ActionListener {
         inputScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         contentPane.add(inputScrollPane);
 
-        //Buttons
+/*        //Buttons
         sendMes = new JButton("Send message");
         sendMes.setBounds(290, 405, 200,25);
         contentPane.add(sendMes);
-        sendMes.addActionListener(this);
+        sendMes.addActionListener(this);*/
     }
 
-    @Override
+/*    @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getSource() == sendMes) {
             String input = inputMes.getText();
+            System.out.println(input);
             inputMes.replaceRange("",0,input.length());
             try {
                 clientListener.forward(input);
@@ -80,7 +84,7 @@ public class ChatGraphic extends JFrame implements ActionListener {
             }
         }
 
-    }
+    }*/
 
     /**
      * Sends Information about valid commands to the client
@@ -124,6 +128,30 @@ public class ChatGraphic extends JFrame implements ActionListener {
             name = "";
         }
         clientListener.pw.println("SETTO§Nickname§" + name);
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+        if (keyEvent.getKeyChar() == KeyEvent.VK_ENTER) {
+            String input = inputMes.getText().replaceAll("\n", " ");
+            inputMes.replaceRange("",0,input.length());
+            try {
+                clientListener.forward(input);
+            } catch (IndexOutOfBoundsException | NotACommandException e) {
+                printErrorMessage(e.getMessage());
+                printCommandInfo();
+            }
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
 
     }
 }

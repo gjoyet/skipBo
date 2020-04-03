707c0fadae11859faddf3aed23dd2829891a7b66
Janni Batsilas <user.email>
2020-04-03 11:45:29 +0200

    Edited Build.gradle: creates now .jar file Edited settings.gradle: changed name of root

--- src/main/java/ch/unibas/enums/ch/unibas/enums/DeckType.java
+++
@@ -1,8 +0,0 @@
-package ch.unibas.enums;
-
-public enum DeckType {
-    BUILD_PILE,
-    DISCARD_PILE,
-    HAND_PILE,
-    DRAW_PILE
-}
--- src/main/java/ch/unibas/enums/ch/unibas/enums/ServerResponse.java
+++
@@ -1,8 +0,0 @@
-package ch.unibas.enums;
-
-public enum ServerResponse {
-    LOGGED_IN,
-    LOGGED_OUT,
-    INVALID_USERNAME,
-
-}

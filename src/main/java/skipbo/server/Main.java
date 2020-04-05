5af189915e07a57a6244767f7cf55c4145b8a3b3
Guillaume <guillaume.joyet@unibas.ch>
2020-04-05 16:27:16 +0200

    Prevented Main to to throw IndexOutOfBoundException because args[2] was empty.

--- src/main/java/skipbo/server/Main.java
+++ src/main/java/skipbo/server/Main.java
@@ -9,7 +9,9 @@ public class Main {
             SBServer sbServer = new SBServer(Integer.parseInt(args[1]));
             Thread serverT = new Thread(sbServer); serverT.start();
         } else if(args[0].equalsIgnoreCase("client")) {
-            String[] arguments = {args[1], args[2]};
+            String[] arguments = new String[2];
+            arguments[0] = args[1];
+            if(args.length == 2) arguments[1] = args[2];
             SBClient sbClient = new SBClient(arguments);
             Thread sbClientT = new Thread(sbClient); sbClientT.start();
         }

90777ed592f71c25b705fb86f95aa4b6e27d4158
Guillaume <guillaume.joyet@unibas.ch>
2020-04-05 16:52:08 +0200

    JAR file seems to finally be working.

--- src/main/java/skipbo/server/Main.java
+++ src/main/java/skipbo/server/Main.java
@@ -1,17 +1,25 @@
 package skipbo.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import skipbo.client.SBClient;
 
 public class Main {
 
+    static Logger mainLog = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        mainLog.debug("Program starting.");
         if(args[0].equalsIgnoreCase("server")) {
+            mainLog.debug("Main triggered Server.");
             SBServer sbServer = new SBServer(Integer.parseInt(args[1]));
             Thread serverT = new Thread(sbServer); serverT.start();
         } else if(args[0].equalsIgnoreCase("client")) {
-            String[] arguments = new String[2];
-            arguments[0] = args[1];
-            if(args.length == 2) arguments[1] = args[2];
+            mainLog.debug("Main triggered Client.");
+            String[] arguments = new String[args.length-1];
+            for(int i=1; i < args.length; i++) {
+                arguments[i-1] = args[i];
+            }
             SBClient sbClient = new SBClient(arguments);
             Thread sbClientT = new Thread(sbClient); sbClientT.start();
         }
